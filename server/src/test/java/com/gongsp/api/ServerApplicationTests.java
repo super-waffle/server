0be5b95633b045d54c11c16b1628bd19fd7e421a
chung1997jy <chung1997jy@gmail.com>
2022-01-25 22:56:02 +0900

    [#6] style: 패키지구조, gradle, application 설정

---
+++ server/src/test/java/com/gongsp/api/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongsp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
