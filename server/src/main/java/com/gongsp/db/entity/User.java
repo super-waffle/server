4868f8ffa56ad1159cff1f58aac94416e0183e2f
itzmeee <itzmeee@naver.com>
2022-01-26 15:35:22 +0900

    [#7] update: check nickname

--- server/src/main/java/com/gongsp/db/entity/User.java
+++ server/src/main/java/com/gongsp/db/entity/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
