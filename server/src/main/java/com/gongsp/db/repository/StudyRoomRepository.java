dfc258265b74c1341809f68fd4a8915775f11c95
조용문 <choymoonk@gmail.com>
2022-02-17 10:47:01 +0900

    hotfix: 마감 스터디 나오는 문제 해결 및 페이지네이션 수정

--- server/src/main/java/com/gongsp/db/repository/StudyRoomRepository.java
+++ server/src/main/java/com/gongsp/db/repository/StudyRoomRepository.java
@@ -12,46 +12,58 @@ import java.util.Optional;
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Integer> {
     Optional<StudyRoom> findStudyByStudySeq(Integer studySeq);
 
+    @Query(nativeQuery = true, value = "select count(*) from tb_study " +
+            "where is_study_recruiting = true " +
+            "and study_recruit_end >= :today ;")
+    int countAllWithoutCount(@Param("today") LocalDate today);
+
     @Query(nativeQuery = true, value = "select * from tb_study " +
             "where is_study_recruiting = true " +
-            "order by study_seq desc limit :start, :spp ")
-    List<StudyRoom> searchAll(@Param("start") Integer start, @Param("spp") Integer spp);
+            "and study_recruit_end >= :today " +
+            "order by study_seq desc limit :start, :spp ;")
+    List<StudyRoom> searchAll(@Param("start") Integer start, @Param("spp") Integer spp, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select count(*) from tb_study " +
             "where is_study_recruiting = true " +
-            "where category_seq = :categorySeq ")
-    int countByCategory(@Param("categorySeq") int categorySeq);
+            "and study_recruit_end >= :today " +
+            "where category_seq = :categorySeq ;")
+    int countByCategory(@Param("categorySeq") int categorySeq, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select * from tb_study " +
             "where category_seq = :categorySeq " +
             "and is_study_recruiting = true " +
-            "order by study_seq desc limit :start, :spp ")
-    List<StudyRoom> searchByCategorySeq(@Param("categorySeq") Integer categorySeq, @Param("start") Integer start, @Param("spp") Integer spp);
+            "and study_recruit_end >= :today " +
+            "order by study_seq desc limit :start, :spp ;")
+    List<StudyRoom> searchByCategorySeq(@Param("categorySeq") Integer categorySeq, @Param("start") Integer start, @Param("spp") Integer spp, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select count(*) from tb_study " +
             "where (study_title like %:key% or study_desc like %:key% or study_short_desc like %:key% ) " +
-            "and is_study_recruiting = true ")
-    int countByLike(@Param("key") String key);
+            "and study_recruit_end >= :today " +
+            "and is_study_recruiting = true ;")
+    int countByLike(@Param("key") String key, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select * from tb_study " +
             "where (study_title like %:key% or study_desc like %:key% or study_short_desc like %:key% ) " +
             "and is_study_recruiting = true " +
-            "order by study_seq desc limit :start, :spp ")
-    List<StudyRoom> searchByKey(@Param("key") String key, @Param("start") Integer start, @Param("spp") Integer spp);
+            "and study_recruit_end >= :today " +
+            "order by study_seq desc limit :start, :spp ;")
+    List<StudyRoom> searchByKey(@Param("key") String key, @Param("start") Integer start, @Param("spp") Integer spp, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select count(*) from tb_study " +
             "where (study_title like %:key% or study_desc like %:key% or study_short_desc like %:key% ) " +
             "and is_study_recruiting = true " +
-            "and category_seq = :categorySeq ")
-    int countByLikeAndCategory(@Param("categorySeq") int categorySeq, @Param("key") String key);
+            "and study_recruit_end >= :today " +
+            "and category_seq = :categorySeq ;")
+    int countByLikeAndCategory(@Param("categorySeq") int categorySeq, @Param("key") String key, @Param("today") LocalDate today);
 
     @Query(nativeQuery = true, value = "select * from tb_study " +
             "where (study_title like %:key% or study_desc like %:key% or study_short_desc like %:key% ) " +
             "and category_seq = :categorySeq " +
             "and is_study_recruiting = true " +
-            "order by study_seq desc limit :start, :spp ")
-    List<StudyRoom> searchByKeyAndCategory(@Param("key") String key, @Param("categorySeq") Integer categorySeq, @Param("start") Integer start, @Param("spp") Integer spp);
+            "and study_recruit_end >= :today " +
+            "order by study_seq desc limit :start, :spp ;")
+    List<StudyRoom> searchByKeyAndCategory(@Param("key") String key, @Param("categorySeq") Integer categorySeq, @Param("start") Integer start, @Param("spp") Integer spp, @Param("today") LocalDate today);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM tb_study WHERE study_recruit_end = :date")
+    @Query(nativeQuery = true, value = "SELECT * FROM tb_study WHERE study_recruit_end <= :date ;")
     List<StudyRoom> findStudyRoomsByStudyDateEnd(@Param("date") LocalDate date);
 }
